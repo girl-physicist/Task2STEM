7beda25b5a6a3f0a3594ec11471c31f80f85e9c8
Anastasiya <girl-physicist@live.com>
2018-10-11 14:09:08 +0300

    Commit1

--- Task2STEM/Form1.Designer.cs
+++ Task2STEM/Form1.Designer.cs
@@ -28,13 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.textBoxSort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMinSugar = new System.Windows.Forms.TextBox();
+            this.textBoxMaxSugar = new System.Windows.Forms.TextBox();
+            this.Find = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ShowInfo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(364, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.Location = new System.Drawing.Point(13, 42);
+            this.textBoxInfo.Multiline = true;
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBoxInfo.Size = new System.Drawing.Size(342, 214);
+            this.textBoxInfo.TabIndex = 2;
+            // 
+            // textBoxSort
+            // 
+            this.textBoxSort.Location = new System.Drawing.Point(364, 43);
+            this.textBoxSort.Multiline = true;
+            this.textBoxSort.Name = "textBoxSort";
+            this.textBoxSort.Size = new System.Drawing.Size(339, 213);
+            this.textBoxSort.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "min sugar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 290);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "max sugar";
+            // 
+            // textBoxMinSugar
+            // 
+            this.textBoxMinSugar.Location = new System.Drawing.Point(71, 257);
+            this.textBoxMinSugar.Name = "textBoxMinSugar";
+            this.textBoxMinSugar.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMinSugar.TabIndex = 6;
+            // 
+            // textBoxMaxSugar
+            // 
+            this.textBoxMaxSugar.Location = new System.Drawing.Point(71, 283);
+            this.textBoxMaxSugar.Name = "textBoxMaxSugar";
+            this.textBoxMaxSugar.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMaxSugar.TabIndex = 6;
+            // 
+            // Find
+            // 
+            this.Find.Location = new System.Drawing.Point(15, 316);
+            this.Find.Name = "Find";
+            this.Find.Size = new System.Drawing.Size(75, 23);
+            this.Find.TabIndex = 7;
+            this.Find.Text = "Find";
+            this.Find.UseVisualStyleBackColor = true;
+            this.Find.Click += new System.EventHandler(this.Find_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 346);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(340, 186);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(715, 527);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Find);
+            this.Controls.Add(this.textBoxMaxSugar);
+            this.Controls.Add(this.textBoxMinSugar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSort);
+            this.Controls.Add(this.textBoxInfo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxInfo;
+        private System.Windows.Forms.TextBox textBoxSort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMinSugar;
+        private System.Windows.Forms.TextBox textBoxMaxSugar;
+        private System.Windows.Forms.Button Find;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Task2STEM/Form1.cs
+++ Task2STEM/Form1.cs
@@ -1,20 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Task2STEM.Classes;
+using Task2STEM.Enums;
+using Task2STEM.Interfaces;
 
 namespace Task2STEM
 {
     public partial class Form1 : Form
     {
+        private IGift _gift;
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Initialize();
+        }
+        public void Initialize()
+        {
+            _gift = new Gift("Gift 1", new List<ISweetness>());
+            Candy candy1 = new Candy("Сhupa Chups XXL 4D", 29, 0.65, 370, TypeOfCandy.Bars);
+            Candy candy2 = new Candy("Аэрофлотские", 150, 64.3, 483, TypeOfCandy.Chocolate);
+            Chocolate choco1 = new Chocolate("Спартак", 75, 48, 540, TypeOfChocolate.Dark);
+            Chocolate choco2 = new Chocolate("Любимая Алёнка", 200, 51.5, 550, TypeOfChocolate.Milk);
+            _gift.AddSweet(candy1);
+            _gift.AddSweet(candy2);
+            _gift.AddSweet(choco1);
+            _gift.AddSweet(choco2);
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("{0}, consist {1} elements", _gift.GiftName, _gift.CountOfSweet);
+            Console.WriteLine();
+            Console.WriteLine("Gift Weight: {0}", _gift.GiftWeight);
+            Console.WriteLine();
+            Console.WriteLine("/-------------------------------------------/");
+            Console.WriteLine();
+
+            foreach (var i in _gift.Items)
+            {
+                Console.WriteLine("{0}, {1}", i.TypeOfSweetness, i.ItemInfo);
+            }
+
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            var temp = _gift.SortSweetnessByWeight();
+            int j = 0;
+            foreach (var i in temp)
+            {
+                listBox1.Items.Insert(j, String.Format("Name: {0}, SugarPerUnit: {1}",
+                   i.Name, i.SugarPerUnit));
+                j++;
+            }
+        }
+        private void Find_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            var min = Convert.ToDouble(textBoxMinSugar.Text);
+            var max = Convert.ToDouble(textBoxMaxSugar.Text);
+            var gifts = _gift.FindSweetnessBySugar(min, max);
+            for (int i =0;i < gifts.Count();i++)
+            {
+                listBox1.Items.Insert(i, String.Format("Name: {0}, SugarPerUnit: {1}",
+                    gifts.ElementAt(i).Name, gifts.ElementAt(i).SugarPerUnit));
+            }
+           
         }
     }
 }
